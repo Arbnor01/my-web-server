6cc2321e15dfb0aabceea8f135c30c231a3c7d45
Arbnor <arbnor-sfarca@hotmail.com>
2023-05-31 12:11:33 +0100

    Added test for POST /sort-names route

--- spec/integration/app_spec.rb
+++ spec/integration/app_spec.rb
@@ -15,4 +15,16 @@ describe Application do
       expect(response.body).to eq("Julia, Mary, Karim")
     end
   end
+
+  context "POST /sort-names" do
+    it 'returns sorted list of names' do
+      names = "Joe,Alice,Zoe,Julia,Kieran"
+      expected_response = "Alice,Joe,Julia,Kieran,Zoe"
+
+      post('/sort-names', names: names)
+
+      expect(last_response.status).to eq(200)
+      expect(last_response.body).to eq(expected_response)
+    end
+  end
 end
